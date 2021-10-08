55772da212b0532cdd357ffe64c3f58bee3a0b9f
“xiba” <1120754072@qq.com>
2021-10-08 11:24:57 +0800

    “day1”

---
+++ day1.py
@@ -0,0 +1,72 @@
+'''
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+'''
+
+print("====================================")
+print("日期 服装名称 价格/件 库存数量 日售量")
+print("1号 羽绒服  253.6   500       10")
+print("2号  牛仔裤  86.3    600      60")
+print("3号  风衣    96.8    335      43")
+print("4号  皮草    135.9  855      63")
+print("5号  T血     65.8    632      63")
+print("6号  衬衫    49.3    562     120")
+print("7号  牛仔裤  86.3    600      72")
+print("8号 羽绒服   253.6   500      69")
+print("9号 牛仔裤   86.3    600      35")
+print("10号 羽绒服  253.6   500     140")
+print("11号 牛仔裤   86.3   600      90")
+print("12号 皮草    135.9   855      24")
+print("13号 T血     65.8    632     45")
+print("14号 风衣    96.8    335      25")
+print("15号 牛仔裤   86.3    600    60")
+print("16号 T血     65.8    632    129")
+print("17号 羽绒服   253.6  500      10")
+print("18号 风衣    96.8    335     43")
+print("19号 T血     65.8    632     63")
+print("20号 牛仔裤   86. 3   600     60")
+print("21号 皮草    135.9   855      63")
+print("22号 风衣    96.8    335      60")
+print("23号 T血     65.8    632     58")
+print("24号 牛仔裤   86.3   600     140")
+print("25号 T血     65.8    632     48")
+print("26号 风衣    96.8    335      43")
+print("27号 皮草    135.9   855      57")
+print("28号 羽绒服   253.6   500     10")
+print("29号 T血     65.8     632     63")
+print("30号 风衣     96. 8   335     78")
+num=(253.6*239+86.3*517+96.8*292+135.9*207+65.8*469+49.3*120)
+print("总售额=",num)
+num=(1844/30)
+print("平均日售量=",num)
+num1=(239/1844*100);num2=(517/1844*100);num3=(292/1844*100);num4=(207/1844*100);num5=(469/1844*100);num6=(120/1844*100)
+print("羽绒服占比=",'{:.2f}%'.format(num1));print("牛仔裤占比=",'{:.2f}%'.format(num2))
+print("风衣占比=",'{:.2f}%'.format(num3) );print("皮草占比=",'{:.2f}%'.format(num4))
+print("T血占比=",'{:.2f}%'.format(num5));print("衬衫占比=",'{:.2f}%'.format(num6))
+
+
+
+
+
+
+
+
+
+
+
+
+
